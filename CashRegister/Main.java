c6c3df7e8274aa5b97e7fd4ef70df0faa915a971
diaseric <42677871+diaseric@users.noreply.github.com>
2019-03-20 17:25:04 -0700

    Create Main.java

---
+++ CashRegister/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Product bonRougePear = new Product("Bon Rouge Pears",3014,false,.69,112,"Produce");
+        Product yellowBanana = new Product("Yellow Bananas",4011,false,.69,98,"Produce");
+    }
+}
